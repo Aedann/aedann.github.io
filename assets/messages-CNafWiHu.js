9b261765332dc0a4ac803dfe2671229dca458e81
Aedann <Aedann@users.noreply.github.com>
2025-10-02 14:34:14 +0000

    deploy: 10a5783806a09ed5c53bc1ed9e603e62402eae1d

---
+++ assets/messages-CNafWiHu.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"6fI2E+":["Petit texte introductif, qui dépend du thème choisi et s’adapte à la couleur globale."],"9K9GTQ":["ans"]}');export{e as messages};
---
+++ assets/messages-gK3HZXrs.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"6fI2E+":["Small Introductive text"],"9K9GTQ":["ans"],"Dj0nYr":["years old"],"Q7knwC":["Small Introductive text"]}');export{t as messages};
